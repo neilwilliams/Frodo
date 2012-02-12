4b576ce6df8f52d571023b27e40fa1711e1b0c01
Neil Williams <neil121183@gmail.com>
2012-02-12 22:07:35 +0000

    First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Frodo::Application.config.secret_token = '08b891134e9fe391a3219a756d58b58f8a2932a2ef2cb47263b82f217ccce814bc9f21e4f4907e42120934cc134311de552f879da52cdc824afa07181bd43abd'
